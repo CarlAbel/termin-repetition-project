164a70e7b7102a57ce6192ba5ee81a23216678f2
CarlAbel <caweb.cph@gmail.com>
2023-02-28 11:39:53 +0100

    Crud Termins Rep

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
 function App() {
+  function handleSubmit() {
+    fetch("http://localhost:3030/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: "hello",
+        body: "world",
+      }),
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label>
+          Title
+          <input type="text" name="title" />
+        </label>
+        <label>
+          description
+          <input type="text" name="body" />
+        </label>
+        <button onClick={handleSubmit} type="submit">
+          Save
+        </button>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
